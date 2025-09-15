f3878e7c618765531ccee1fb3c337fc6bab4362e
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-09-15 19:15:25 +0530

    Initial commit

---
+++ Backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDb =async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        console.log("Database connected");
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+        
+export default connectDb;
