41a01bbc81ea8ac95aa1fad2801ebafdd15d4a2f
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-09-17 22:43:01 +0530

    push

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { getCurrentUsers } from "../controllers/user.controllers.js";
+import { isAuth } from './../middlewares/isAuth.js';
 
 const userRouter = express.Router()
 
-userRouter.get("/current", getCurrentUsers)
+userRouter.get("/current",isAuth, getCurrentUsers)
 
 export default userRouter;
--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -1,9 +1,27 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect } from 'react'
 export const userDataContext = createContext()
+import axios from "axios"
 
 function UserContext({children}) {
 
     const serverUrl = "http://localhost:3000"
+    const [userData, setUserData] = React.useState(null)
+
+    const handleCurrentUser = async () => {
+      try {
+        const result = await axios.get(`${serverUrl}/api/user/
+          currentUser`, {withCredentials: true})
+        setUserData(result.data)
+        console.log(result.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    useEffect(() => {
+      handleCurrentUser()
+    }, [])
+
     const value = {
         serverUrl
     }
